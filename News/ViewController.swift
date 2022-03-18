89797e5e17ff3f9eb10c500038ac6192ab3e8f1c
Tomas Fernandez Velazco <tfernandez@bemobile.es>
2022-03-18 19:43:02 +0100

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Tomas Fernandez Velazco on 18/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
